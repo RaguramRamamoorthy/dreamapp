963f144c02081da94337dda98c1102547799d8a4
ragu <rrrtechie@gmail.com>
2021-11-10 02:51:52 +0530

    new changes

--- app.py
+++ app.py
@@ -6,16 +6,15 @@ import webbrowser
 
 st.title("Brain Tumor MRI Classification")
 
-link = '[Kaggle-link for MRI images](https://www.kaggle.com/navoneel/brain-mri-images-for-brain-tumor-detection)'
+
+st.text("If you need MRI images for testing the App")
+st.text("Copy paste this link in a browser ")
+st.text("https://www.kaggle.com/navoneel/brain-mri-images-for-brain-tumor-detection")
 
 
-st.text("If you need MRI images for testing the App,please find it in the kaggle link below")
-st.markdown(link, unsafe_allow_html=True)
 
 
-
-st.text("Upload a brain MRI Image for image classification to find weather the scan image")
-st.text("is healthy or not")
+st.text("Upload a brain MRI Image to find weather it is healthy or not")
 
 uploaded_file = st.file_uploader("Choose a brain MRI ...")
 if uploaded_file is not None:
