d789de919992a7db2b1192387ae64febb8f72005
ragu <rrrtechie@gmail.com>
2021-11-10 18:53:52 +0530

    position changes

--- app.py
+++ app.py
@@ -3,26 +3,9 @@ from PIL import Image, ImageOps
 from img_classification import teachable_machine_classification
 import os
 
-
-
 st.title("Brain Tumor MRI Classification")
 
-
-st.text("If you need MRI images for testing the App")
-st.text("download a zip folder from here...")
-
-# folder = os.getcwd() + '/images'
-# st.download_button('Download images', folder)
-
-with open("images.zip", "rb") as fp:
-    btn = st.download_button(
-        label="Download ZIP",
-        data=fp,
-        file_name="images.zip",
-        mime="application/zip"
-    )
-
-st.text("Upload a brain MRI Image to find weather it is healthy or not")
+st.text("Upload a brain MRI Image here to find weather it is healthy or not")
 
 uploaded_file = st.file_uploader("Choose a brain MRI ...",type=["png","jpg","jpeg"])
 if uploaded_file is not None:
@@ -36,9 +19,21 @@ if uploaded_file is not None:
     else:
         st.write("The MRI scan has a brain tumor")
 
+st.text("If you need MRI images for testing the App")
+st.text("download a zip folder from here...")
+
+with open("images.zip", "rb") as fp:
+    btn = st.download_button(
+        label="Download ZIP",
+        data=fp,
+        file_name="images.zip",
+        mime="application/zip"
+    )
+
 
 st.text("If you need assistance in deploying a medicine based machine learning or a deep")
 st.text("learning model that might be useful for the general popualtion or a specific")
-st.text("hospital please reach out to us")
-st.text("Contact me at 'rrrtechie@gmail.com'")
+st.text("hospital please feel free to reach out to us....")
+
+st.text("Contact me at 'rrrtechie@gmail.com' for feedback or enquiries")
 
